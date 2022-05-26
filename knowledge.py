fafc111203b24850eb4dc5376cbdba44ce341754
Daniele Cusentino <pfarrd@gmail.com>
2022-05-26 14:10:19 -0400

    new knowledge file for class

---
+++ knowledge.py
@@ -0,0 +1,16 @@
+#a statement that prints "hello world!"
+print("Hello World!")
+
+#make a list populated with several values, then print the list. 
+favorite_movies = ['Clue', 'A Christmas Story', 'Beetlejuice', 'Clueless']
+print(favorite_movies[3])
+
+#dictionary with two keys and two values. Print one of the values.
+acronyms = {}
+acronyms['AWOL'] = 'Absent Without Leave'
+acronyms ['BRB'] = 'Be Right Back'
+print(acronyms['BRB'])
+
+#a tuple with 4 values. Print one.
+numbers = (1, 2, 3, 4)
+print(numbers[2])
